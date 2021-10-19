4d0e2cac1e1b1dd167824ba560ceb28d96e158c2
antikovt <antikovt@gmail.com>
2021-10-19 22:06:50 +0300

    Написан код по Виженеру

--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -9,8 +9,23 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     >>> encrypt_vigenere("ATTACKATDAWN", "LEMON")
     'LXFOPVEFRNHR'
     """
-    ciphertext = ""
-    # PUT YOUR CODE HERE
+    keyword = keyword.lower()
+    keyword = (keyword * (len(plaintext) // len(keyword) + 1))[: len(plaintext)]
+    keyword = list(map(ord, keyword))
+    plaintext = list(map(ord, plaintext))
+    for i in range(len(plaintext)):
+        shift = keyword[i] - 97
+        if 65 <= plaintext[i] <= 90:
+            plaintext[i] += shift
+            while plaintext[i] > 90:
+                plaintext[i] -= 26
+        elif 97 <= plaintext[i] <= 122:
+            plaintext[i] += shift
+            while plaintext[i] > 122:
+                plaintext[i] -= 26
+        plaintext[i] = chr(plaintext[i])
+    line = ""
+    ciphertext = line.join(plaintext)
     return ciphertext
 
 
@@ -25,6 +40,21 @@ def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     >>> decrypt_vigenere("LXFOPVEFRNHR", "LEMON")
     'ATTACKATDAWN'
     """
-    plaintext = ""
-    # PUT YOUR CODE HERE
+    keyword = keyword.lower()
+    keyword = (keyword * (len(ciphertext) // len(keyword) + 1))[: len(ciphertext)]
+    keyword = list(map(ord, keyword))
+    ciphertext = list(map(ord, ciphertext))
+    for i in range(len(ciphertext)):
+        shift = keyword[i] - 97
+        if 65 <= ciphertext[i] <= 90:
+            ciphertext[i] -= shift
+            while ciphertext[i] < 65:
+                ciphertext[i] += 26
+        elif 97 <= ciphertext[i] <= 122:
+            ciphertext[i] -= shift
+            while ciphertext[i] < 97:
+                ciphertext[i] += 26
+        ciphertext[i] = chr(ciphertext[i])
+    line = ""
+    plaintext = line.join(ciphertext)
     return plaintext
