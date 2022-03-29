a4716cfedd6d4c2daee9132abdd72192b4e217df
Kovtoniuk Anton [334739] <76940779+antikovt@users.noreply.github.com>
2022-03-29 14:38:22 +0300

    Исправил isort (?)

---
+++ homework05/tests/research/age.py
@@ -0,0 +1,29 @@
+import datetime as dt
+import statistics
+import typing as tp
+
+from vkapi.friends import get_friends
+
+
+def age_predict(user_id: int) -> tp.Optional[float]:
+    """
+    Наивный прогноз возраста пользователя по возрасту его друзей.
+
+    Возраст считается как медиана среди возраста всех друзей пользователя
+
+    :param user_id: Идентификатор пользователя.
+    :return: Медианный возраст пользователя.
+    """
+    ages = []
+    current_time = dt.date.today().year
+    friends = get_friends(user_id, fields=["bdate"]).items
+    for friend in friends:
+        try:
+            bdate = dt.datetime.strptime(friend["bdate"], "%d.%m.%Y").year  # type: ignore
+        except (KeyError, ValueError):
+            continue
+        ages.append(current_time - bdate)
+    try:
+        return statistics.median(ages)
+    except statistics.StatisticsError:
+        return None
---
+++ homework05/tests/vkapi/config.py
@@ -0,0 +1,7 @@
+# https://hackernoon.com/4-ways-to-manage-the-configuration-in-python-4623049e841b
+
+VK_CONFIG = {
+    "domain": "https://api.vk.com/method",
+    "access_token": "6338d80a9ec28a59d6b272970f98d793b93f13f70668d0a35588bdd83f6bd197aa9033b1076da6d6dc893",
+    "version": "5.126",
+}
